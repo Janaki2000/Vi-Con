ea37225796140579875a503aaf03e60fd0feb76c
Janakiram <mjanakiram62@gmail.com>
2019-10-14 10:50:32 +0530

    Basic layout of app

--- app/src/main/java/com/example/vi_con/ui/main/PlaceholderFragment.java
+++ app/src/main/java/com/example/vi_con/ui/main/PlaceholderFragment.java
@@ -1,9 +1,11 @@
 package com.example.vi_con.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,15 +14,18 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.vi_con.MainActivity;
 import com.example.vi_con.R;
+import com.google.android.material.snackbar.Snackbar;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class PlaceholderFragment extends Fragment {
+    public int position;
 
     private static final String ARG_SECTION_NUMBER = "section_number";
-
+    private Button login_btn;
     private PageViewModel pageViewModel;
 
     public static PlaceholderFragment newInstance(int index) {
@@ -40,6 +45,9 @@ public class PlaceholderFragment extends Fragment {
             index = getArguments().getInt(ARG_SECTION_NUMBER);
         }
         pageViewModel.setIndex(index);
+
+
+
     }
 
     @Override
@@ -51,9 +59,43 @@ public class PlaceholderFragment extends Fragment {
         pageViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                position=Integer.parseInt(s);
+                if(position==1)
+                {
+                    textView.setText("FACULTY LOGIN");
+                }
+                else
+                {
+                    textView.setText("ADMIN LOGIN");
+                }
             }
         });
+
+        login_btn=(Button)root.findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(position==1)
+                {
+                    Snackbar.make(view, "Logging in as Faculty Member", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                   Intent i=new Intent(PlaceholderFragment.this.getActivity(),Navigate.class);
+                   startActivity(i);
+                }
+
+                else
+                {
+
+                    Snackbar.make(view, "Logging in as Admin", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+
+
+            }
+        });
+
+
         return root;
     }
+
 }
